c4e2db623c136007d0671975d72cdbeaf77e3965
Blandus1 <blandine0045@gmail.com>
2025-11-21 11:01:19 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Task from './Task'
 
 function App() {
 
   return (
     <>
-      
+      <Task/>
     </>
   )
 }
---
+++ src/Task.jsx
@@ -0,0 +1,14 @@
+
+
+function Task() {
+
+  return (
+    <>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+     </h1>
+    </>
+  )
+}
+
+export default Task;
