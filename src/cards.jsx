8eb12f8dafd69eef9b86df44c49ab954d6c1b784
Blandus1 <blandine0045@gmail.com>
2025-11-23 23:40:02 +0200

    refining the cards file

--- src/cards.jsx
+++ src/cards.jsx
@@ -2,34 +2,34 @@
 function Cards(){
     return(
         <>
-        <div class="flex  w-600 h-[189.22px] absolute top-260 left-[185.58px]">
-            <div class="w-58 h-45 bg-[#FFFFFF] border">
+        <div class="flex  w-600 h-[189.22px] absolute top-190 left-50 ">
+            <div class="w-58 h-45 bg-[#FFFFFF] border pl-4">
                 <p class="font-bold " ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
 </svg>
 Member First</p>
                 <p><span class="font-bold text-blue-600">13 Million</span> of Rwandans are covered by RSSB services</p>
             </div >
-                <div class="w-58 h-45 absolute left-50 bg-[#FFFFFF] border">
+                <div class="w-60 h-45 absolute left-55 bg-[#FFFFFF] border pl-4">
                 <p class="font-bold"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
 </svg>
 Experience</p>
                 <p><span class="font-bold text-blue-600">60 Years</span> of services for your safety and well-being.</p>
             </div>
-                <div class="w-50 h-45 absolute left-100 bg-[#FFFFFF] border">
+                <div class="w-60 h-45 absolute left-110 bg-[#FFFFFF] border pl-4">
                 <p class="font-bold"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 </svg>
 Branches</p>
                 <p>Our services decentralized in <span class="font-bold text-blue-600">30 branches</span> for your satisfaction</p>
             </div>
-                <div  class="w-50 h-45 absolute left-150 bg-[#FFFFFF] border">
-                <p class="font-bold"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <div  class="w-60 h-45 absolute left-162 bg-[#FFFFFF] border pl-4">
+                <p class="font-bold "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
 </svg>
 Data Driven</p>
-                <p>Learning from our data to improve the services offered to our members</p>
+                <p >Learning from our data to improve the services offered to our members</p>
             </div>
 
         </div>
